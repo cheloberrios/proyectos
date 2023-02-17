fcbf085cfa0060b42a98408bbd8dc71c49865c5e
cheloberrios <contacto.marceloberrios@gmail.com>
2023-02-16 23:12:22 -0300

    continuacion de tarea

---
+++ Coding Dojo/intro_python/suma total.py
@@ -0,0 +1,8 @@
+#Suma total - Crea una función que tome una lista y devuelva la suma de todos los valores en el arreglo.
+
+def total(lista):
+    suma=0
+    for i in range(lista):
+        suma = suma+i
+        print(suma)
+    print(suma)
